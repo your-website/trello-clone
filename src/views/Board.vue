7b800208b7c5d0f11e4ccf9328cbee5e375a42ef
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-04 18:09:15 +0700

    add drag and drop columns

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -5,9 +5,11 @@
         class="column"
         v-for="(column, $columnIndex) of board.columns"
         :key="$columnIndex"
-        @drop="moveTask($event, column.tasks)"
+        draggable
+        @drop="moveTaskOrColumn($event, column.tasks, $columnIndex)"
         @dragover.prevent
         @gragenter.prevent
+        @dragstart.self="pickupColumn($event, $columnIndex)"
       >
         <div class="flex items-center mb-2 font-bold">
           {{ column.name }}
@@ -20,6 +22,9 @@
             draggable
             @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
             @click="goToTask(task)"
+            @dragover.prevent
+            @gragenter.prevent
+            @drop.stop="moveTaskOrColumn($event, column.tasks, $columnIndex, $taskIndex)"
           >
             <span class="w-full flex-no-shrink font-bold">{{ task.name }}</span>
             <p
@@ -50,66 +55,79 @@ import { mapState } from "vuex";
 export default {
   computed: {
     ...mapState(["board"]),
-    isTaskOpen() {
+    isTaskOpen () {
       return this.$route.name === "task";
-    },
+    }
   },
   methods: {
-    goToTask(task) {
+    goToTask (task) {
       this.$router.push({ name: "task", params: { id: task.id } });
     },
-    closeTask() {
+    closeTask () {
       this.$router.push({ name: "board" });
     },
-    createTask(e, tasks) {
+    createTask (e, tasks) {
       this.$store.commit("CREATE_TASK", {
         tasks,
-        name: e.target.value,
-      });
+        name: e.target.value
+      })
       e.target.value = "";
     },
-    pickupTask(e, taskIndex, fromColumnIndex) {
+    pickupTask (e, taskIndex, fromColumnIndex) {
       e.dataTransfer.effectAllowed = "move";
       e.dataTransfer.dropEffect = "move";
 
-      e.dataTransfer.setData("task-index", taskIndex);
+      e.dataTransfer.setData("from-task-index", taskIndex);
       e.dataTransfer.setData("from-column-index", fromColumnIndex);
-
-      // this.$store.commit("MOVE_TASK", {
-      //   fromColumn: this.board.columns[fromColumnIndex].tasks,
-      //   toColumn: e.target,
-      //   taskIndex: taskIndex,
-      // });
+      e.dataTransfer.setData("type", "task");
     },
-    moveTask(e, toTasks) {
+    pickupColumn (e, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.dropEffect = "move";
+
+      e.dataTransfer.setData("from-column-index", fromColumnIndex);
+      e.dataTransfer.setData("type", "column");
+    },
+    moveTaskOrColumn (e, toTask, toColumnIndex, toTaskIndex) {
+      const type = e.dataTransfer.getData('type')
+
+      if (type === 'task') {
+        this.moveTask(e, toTask, toTaskIndex !== undefined ? toTaskIndex : toTask.length)
+      } else {
+        this.moveColumn(e, toColumnIndex)
+      }
+    },
+    moveTask (e, toTasks, toTaskIndex) {
       const fromColumnIndex = e.dataTransfer.getData("from-column-index");
       const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const taskIndex = e.dataTransfer.getData("task-index");
-
+      const fromTaskIndex = e.dataTransfer.getData("from-task-index");
       this.$store.commit("MOVE_TASK", {
         fromTasks,
         toTasks,
-        taskIndex,
-      });
+        fromTaskIndex,
+        toTaskIndex
+      })
     },
-  },
-};
+    moveColumn (e, toColumnIndex) {
+      const fromColumnIndex = e.dataTransfer.getData("from-column-index");
+
+      this.$store.commit('MOVE_COLUMN', { fromColumnIndex, toColumnIndex })
+    }
+  }
+}
 </script>
 
 <style lang="css">
 .task {
   @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
 }
-
 .column {
   @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
   min-width: 350px;
 }
-
 .board {
   @apply p-4 bg-teal-dark h-full overflow-auto;
 }
-
 .task-bg {
   @apply pin absolute;
   background: rgba(0, 0, 0, 0.5);
